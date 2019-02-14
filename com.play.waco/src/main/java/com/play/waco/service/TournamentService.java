13cf4037b1701375e9ae5604116746c359647d67
scomnet <scoment@homeplus.co.kr>
2019-02-14 16:54:48 +0900

    gymid 별로 player 분리

--- com.play.waco/src/main/java/com/play/waco/service/TournamentService.java
+++ com.play.waco/src/main/java/com/play/waco/service/TournamentService.java
@@ -1,7 +1,9 @@
 package com.play.waco.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -26,12 +28,61 @@ public class TournamentService {
 		
 		List<Tournament> playerList = new ArrayList<Tournament>();
 		playerList = tournamentMapper.selectMatchPlayerInfo(dto.toEntity());
-		log.info("payerList : {}", playerList.get(0).getGymid());
-		//tournamentMapper.selectMatchPlayerInfo(dto.toEntity());
-		//log.info("List : {} ", tournamentMapper.selectMatchPlayerInfo(dto.toEntity()).get(7).getWeight());
+
+		tournamentMap(playerList);
+		/*
+		for( Tournament player : playerList ) {
+			log.info("playerid : {}", player.getPlayerid());
+		}
+		*/
+		
 	}
-	
-	
-	
+
+	/*
+	 * mapping process
+	 * out put : boolean
+	 */
+	public void tournamentMap(List<Tournament> playerList) {
+
+		/* GYM 별 선수 분리 후 저장할 List */
+		Map<Integer, List<Tournament>> playerForGYM = new HashMap<Integer, List<Tournament>>();
+		List<Tournament> tempPlayer = null;
+		
+		/* for문에서 사용하는 List index 변수 */
+		int i = 0;
+		int arrayNo = 0;
+		int mapKey = 0;
+		
+		/*
+		 * DB에서 GYMID(체육관 id)와 WEIGHT_DIVISION(체급) 으로 sort : asc
+		 * List => Map으로 GYM 별로 분리
+		 */
+		for( Tournament player : playerList ) {
+
+			if( arrayNo != player.getGymid() ) {
+
+				if( arrayNo != 0 ) {
+
+					playerForGYM.put(mapKey, tempPlayer);
+					mapKey++;
+
+				}
+
+				tempPlayer = new ArrayList<Tournament>();
+				
+			} 
+
+			i++;
+			tempPlayer.add(player);
+
+			if( i == playerList.size() ) {
+				playerForGYM.put(mapKey, tempPlayer);
+			}
+			
+			arrayNo = player.getGymid();
+
+		}
+
+	}
 	
 }
